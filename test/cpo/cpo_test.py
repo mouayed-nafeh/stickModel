d632e71e525c04ac5989dd314ec1ee7504cc5ead
Al Mouayed Bellah Nafeh <mouayed.nafeh@iusspavia.it>
2024-02-07 13:18:21 +0100

    Cleaning up of example runs

--- test/cpo/cpo_test.py
+++ test/cpo/cpo_test.py
@@ -103,53 +103,82 @@ F = list(pd.read_csv(f'{gitDir}/{structTypo}.csv').iloc[:,1])
 #     plt.xlim([-0.2,0.2])
 
 
-#%% Test NLTHA
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 16 10:57:21 2024
 
-dt_gm = 0.01 
-t_max = 300.00 
-dt_ansys = 0.05 
-sf = 1.00
-test_directory = 'C:/Users/Moayad/Desktop/python/stick-model/test'
-fnames = f'{test_directory}/record.txt'
+@author: Moayad
+"""
 
-model = stickModel(nst,flh,flm,fll,fla,structTypo,gitDir)
-model.mdof_initialise()
-model.mdof_nodes()
-model.mdof_fixity()
-model.mdof_loads()
-model.mdof_material()
-model.plot_model()
-model.do_gravity_analysis()
-model.do_modal_analysis()
-control_nodes, coll_index, peak_drift, peak_accel, max_peak_drift, max_peak_drift_dir, max_peak_drift_loc, max_peak_accel, max_peak_accel_dir, max_peak_accel_loc, peak_disp = model.do_nrha_analysis(fnames, dt_gm, sf, t_max, dt_ansys, 5)
-    
+#%% Load dependencies
 
-# plot acceleration profiles (in x)
-plt.plot(peak_accel[:,0], control_nodes, color = 'blue',linestyle='solid')
-plt.xlabel("Peak Floor Acceleration, PFA [g]")
-plt.ylabel("Floor No.")
-plt.grid(visible=True, which='major')
-plt.grid(visible=True, which='minor')
-plt.xlim([0,1])
-plt.ylim([0,np.max(control_nodes)])
-plt.show()
+# define the path to input file
+gitDir = 'C:/Users/Moayad/Documents/GitHub/stickModel/'
 
-# plot drift profiles (in x)
+# some_file.py
+import sys
+# caution: path[0] is reserved for script path (or '' in REPL)
+sys.path.insert(1, f'{gitDir}')
 
-bplot_drift, bplot_nodes = duplicate_for_drift(peak_drift[:,0], control_nodes)
-plt.plot(bplot_drift, bplot_nodes, color = 'blue',linestyle='solid')
-plt.xlabel("Peak Storey Drift, PSD [%]")
-plt.ylabel("Floor No.")
-plt.grid(visible=True, which='major')
-plt.grid(visible=True, which='minor')
-plt.show()
+import pandas as pd
+import numpy as np
+from stickModel import stickModel
+import matplotlib.pyplot as plt
+import openseespy.opensees as ops
+from utils import *
 
-# plot drift profiles (in y)
+#%% User input
 
-bplot_drift, bplot_nodes = duplicate_for_drift(peak_drift[:,1], control_nodes)
-plt.plot(bplot_drift, bplot_nodes, color = 'blue',linestyle='solid')
-plt.xlabel("Peak Storey Drift, PSD [%]")
-plt.ylabel("Floor No.")
-plt.grid(visible=True, which='major')
-plt.grid(visible=True, which='minor')
-plt.show()
+
+# read the input file
+input_data = pd.read_excel(f'{gitDir}/test/nltha/input_file.xlsx', sheet_name='Basic')
+
+# read the input parameters 
+nst = int(input_data['Value'][0]) # number of storeys
+flh = [float (i) for i in input_data['Value'][1].split()]
+flm = [float (i) for i in input_data['Value'][2].split()]
+fll = [float (i) for i in input_data['Value'][3].split()]
+fla = [float (i) for i in input_data['Value'][4].split()]
+flm = [1.0, 1.0, 1.0]
+
+# test size
+if len(flh)!=nst or len(flm)!=nst or len(fll)!=nst or len(fla)!=nst:
+    raise ValueError('Number of entries exceed the number of storeys')
+
+# define the structural typology
+structTypo = 'CR_LFINF+CDH+DUL_H1'
+
+#%% Test SPO
+
+ref_disp = 0.002 # Reference displacement (approximately the yield displacement)
+numCycles = 5 # Number of cycles 
+push_dir = 1     # Direction of pushover
+dispIncr = 500   # Displacement increment 
+mu_list = [0.5, 1.0, 2.0, 4.0, 5.0]   # List of target ductility
+
+for i in range(len(mu_list)):
+
+    # compile the model for every analysis object
+    ops.wipe()
+    model = stickModel(nst,flh,flm,fll,fla,structTypo,f'{gitDir}/raw/ip')
+    model.mdof_initialise()
+    model.mdof_nodes()
+    model.mdof_fixity()
+    model.mdof_loads()
+    model.mdof_material()
+    model.plot_model()
+    model.do_gravity_analysis()
+    model.do_modal_analysis()
+    ops.wipeAnalysis()
+
+    # do the analysis
+    cpo_disps, cpo_rxn = model.do_cpo_analysis(ref_disp, mu_list[i], numCycles, push_dir, dispIncr, pflag = False)
+    # plot the results
+    plt.plot(cpo_disps[:,-1], cpo_rxn, color = 'blue',linestyle='dashed')
+    plt.xlabel("Top Displacement, $\delta$ [m]")
+    plt.ylabel("Base Shear, V [kN]")
+    plt.grid(visible=True, which='major')
+    plt.grid(visible=True, which='minor')
+    plt.xlim([-0.01,0.01])
+
+
